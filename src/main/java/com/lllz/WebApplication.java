ff67764ffe38a01517a56229728621f947926435
zxx <1791659337@qq.com>
2020-10-16 17:32:12 +0800

    第二次 修改了main里面添加输出语句

--- src/main/java/com/lllz/WebApplication.java
+++ src/main/java/com/lllz/WebApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(WebApplication.class, args);
+        System.out.println(123);
     }
 
 }
